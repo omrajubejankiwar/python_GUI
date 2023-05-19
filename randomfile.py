de18e6db29da4d6c663f958485cc910eb9b4849c
omrajubejankiwar <131599536+omrajubejankiwar@users.noreply.github.com>
2023-05-19 14:31:20 +0530

    Add files via upload

---
+++ randomfile.py
@@ -0,0 +1,9 @@
+import random
+
+list1=["1","2","3","4","5"]
+
+print(random.random()*(20-10)+10)
+print(random.randint(30,50))
+print(random.randrange(5))
+print(random.uniform(2,5))
+print(random.choice(list1))
